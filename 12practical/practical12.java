315fe9721f1c1f1f94d3dead85f4781590a285f2
Isack <4460799@myuwc.ac.za>
2026-02-16 15:46:54 +0200

    Converted the 2nd On2 algorithm to Java

--- 12practical/practical12.java
+++ 12practical/practical12.java
@@ -36,5 +36,23 @@ public class practical12 {
         return maxsofar;
     }
 
+    public static int mcs0n2B(int[] X) {
+        int n = X.length;
+        int[] sumTo = new int[n + 1];
+        sumTo[0] = 0;
+        for (int i = 0 ; i < n; i++) {
+            sumTo[i+1] = sumTo[i] + X[i];
+        }
+        int maxsofar = 0;
+        for (int low = 0; low < n ; low++){
+            for (int high = low; high < n; high++) {
+                int sum = sumTo[high +1] - sumTo[low];
+                if (sum > maxsofar){
+                    maxsofar = sum;
+                }
+            }
+        }
+        return maxsofar;
+    }
 
 }
