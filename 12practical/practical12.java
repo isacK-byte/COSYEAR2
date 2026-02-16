2fee6b718cc7e5575fca2c2449370862e4f345e1
Isack <4460799@myuwc.ac.za>
2026-02-16 15:56:13 +0200

    Convereted the last linear algorithm to Java

--- 12practical/practical12.java
+++ 12practical/practical12.java
@@ -55,4 +55,15 @@ public class practical12 {
         return maxsofar;
     }
 
+    public static int mcs0n(int[] X) {
+        int n = X.length;
+        int maxsofar = 0;
+        int maxtohere = 0;
+        for (int i = 0; i < n; i++) {
+            maxtohere = Math.max(maxtohere+ X[i], 0);
+            maxsofar = Math.max(maxsofar, maxtohere);
+        }
+        return maxsofar;
+    }
+
 }
