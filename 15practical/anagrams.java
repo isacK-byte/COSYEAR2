1d86ef9ecd9d9d18b4335f7cdfc7926db085b53f
Isack Habimana <74471677+isacK-byte@users.noreply.github.com>
2026-03-09 13:17:21 +0000

    Setting inputfile

--- 15practical/anagrams.java
+++ 15practical/anagrams.java
@@ -13,7 +13,15 @@ public class Anagrams{
     }
 
     public static String cleanWord(String original){
-        String w = original.replaceAll("^[^a-zA-Z']+","").replaceAll("[^a-zA-Z']+$","";);
+        String w = original.replaceAll("^[^a-zA-Z']+","").replaceAll("[^a-zA-Z']+$","");
         return w.toLowerCase();
+    }
+
+    public static void main(String[] args){
+
+        String inputFile =  "joyce1922_ulysses.text";
+        System.out.println("reading from file:"+" "+inputFile);
+
+        
     }
 }
