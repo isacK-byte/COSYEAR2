3bbda6d75e00e1ed1798779ccc79e51bc5729181
Isack Habimana <74471677+isacK-byte@users.noreply.github.com>
2026-03-23 12:41:04 +0000

    finished accessors and mutators for tNode

--- 17practical/tryBST.java
+++ 17practical/tryBST.java
@@ -4,13 +4,14 @@ import java.util.*;
 
 public class tryBST
 {
+    //NODE SETUP
   public class tNode
   {
     private long key;
     private String data;
     private tNode left, parent, right ;
 
-    public tNode()
+    public tNode()                      //construct w/o params
     {
         key = 0;
         data = "";
@@ -18,5 +19,25 @@ public class tryBST
         left = null;
         right = null;
     }
+
+    public tNode(long k, String d)      //constructor with params
+    {
+        key = k;
+        data = d;
+        parent = null; 
+        left = null;
+        right = null;
+    }
+
+    public long getKey(){ return key;}  //getters
+    public void setKey(long k){key = k;} //setters
+    public String getData(){ return data;}
+    public void setData(String d){data = d;}
+    public tNode getLeft(){return left;}
+    public void setLeft(tNode n){left = n;}
+    public tNode getRight(){return right;}
+    public void setRight(tNode n){right = n;}
+    public tNode getParent(){return parent;}
+    public void setParent(tNode n){parent = n;}
   }  
 }
