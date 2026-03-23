205f2565a1def2fbe1d0996f1398df6e93f71a75
Isack Habimana <74471677+isacK-byte@users.noreply.github.com>
2026-03-23 12:25:16 +0000

    starting implementing procedures for BST, starting with tNode

--- 17practical/tryBST.java
+++ 17practical/tryBST.java
@@ -4,5 +4,19 @@ import java.util.*;
 
 public class tryBST
 {
-    
+  public class tNode
+  {
+    private long key;
+    private String data;
+    private tNode left, parent, right ;
+
+    public tNode()
+    {
+        key = 0;
+        data = "";
+        parent = null; 
+        left = null;
+        right = null;
+    }
+  }  
 }
