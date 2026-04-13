2d6ad781ae026705587e7dd801cecf608a3e0fd2
isacK-byte <4460799@myuwc.ac.za>
2026-04-13 14:47:11 +0200

    Generic getters and setter for binary Tree Node

--- 18practical/BinaryTreeNode.java
+++ 18practical/BinaryTreeNode.java
@@ -1,3 +1,6 @@
+
+import java.util.function.BinaryOperator;
+
 // 4460799 Monday Practical 1A 2026/04/13
 
 class BinaryTreeNode<E> {               //Q1.2 Added "private " keyword for encapsulation
@@ -5,16 +8,39 @@ class BinaryTreeNode<E> {               //Q1.2 Added "private " keyword for enca
     private BinaryTreeNode<E> left;
     private BinaryTreeNode<E> right;
 
-    public BinaryTreeNode(){
+    public BinaryTreeNode(){            //general constructor 
         this.value = null;
         this.left = null;
         this.right = null;
     } 
+    public BinaryTreeNode(E value, BinaryTreeNode<E> left, BinaryTreeNode<E> right){ // Constructor with paraemeters Q1.3
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
 
+    public void setValue( E value){     //generic setters placed 
+        this.value = value ;
+    }
+    public void setLeft(BinaryTreeNode<E> left){ this.left = left;}
+    public void setRight(BinaryTreeNode<E> right){ this.right = right;}
+    public E getValue(){ return value;} //generic getters placed
+    public BinaryTreeNode<E> getLeft(){return left;}    
+    public BinaryTreeNode<E> getRight(){return right;} 
 }
 
 class BinaryTree<E>{
    private int size;
     private BinaryTreeNode<E> root;
+
+    public BinaryTree(){        // General constructor 
+        this.root = null;
+        size = 0;
+    }
+
+    public BinaryTree(BinaryTreeNode<E> root, int size){            //constructor with parameters
+        this.root = root;
+        this.size = size;
+    }
 
 }
