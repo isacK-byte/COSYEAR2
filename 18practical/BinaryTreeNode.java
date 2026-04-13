e6030cacef260a5db945c52bcfa8736906f90510
isacK-byte <4460799@myuwc.ac.za>
2026-04-13 14:11:20 +0200

    Q1.,1 and Q1.2, code copied and encapsulated

---
+++ 18practical/BinaryTreeNode.java
@@ -0,0 +1,14 @@
+// 4460799 Monday Practical 1A 2026/04/13
+
+class BinaryTreeNode<E> {               //Q1.2 Added "private " keyword for encapsulation
+    private E value;
+    private BinaryTreeNode<E> left;
+    private BinaryTreeNode<E> right;
+
+}
+
+class BinaryTree<E>{
+    int size;
+    BinaryTreeNode<E> root;
+
+}
