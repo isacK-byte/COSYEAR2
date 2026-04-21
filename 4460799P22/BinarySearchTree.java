52c4e2f297856233f56b81367bcb392098051aac
isacK-byte <4460799@myuwc.ac.za>
2026-04-21 22:40:03 +0200

    Just moved the Node class lower. Bothered me looking at it

--- 4460799P22/BinarySearchTree.java
+++ 4460799P22/BinarySearchTree.java
@@ -1,31 +1,4 @@
 //Binary Search Tree class
- class Node {
-    private int data;
-    private Node left;
-    private Node right;
-
-    public Node(){
-        this.data = 0;          //default constructor
-        this.left = null;
-        this.right = null;
-
-    }
-
-    public Node(int data, Node left, Node right){   //loaded constructor
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
-
-    public void setData(int data){this.data = data;}    //setters
-    public void setLeft(Node left){this.left = left;}
-    public void setRight(Node right){this.right = right;}
-                                            //getters
-    public int getData(){return data;}
-    public Node getLeft(){return left;}
-    public Node getRight(){return right;}
-}
-
 
 class BinarySearchTree {
     private Node root;
@@ -70,4 +43,42 @@ class BinarySearchTree {
 
     
 
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+class Node {
+    private int data;
+    private Node left;
+    private Node right;
+
+    public Node(){
+        this.data = 0;          //default constructor
+        this.left = null;
+        this.right = null;
+
+    }
+
+    public Node(int data, Node left, Node right){   //loaded constructor
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void setData(int data){this.data = data;}    //setters
+    public void setLeft(Node left){this.left = left;}
+    public void setRight(Node right){this.right = right;}
+                                            //getters
+    public int getData(){return data;}
+    public Node getLeft(){return left;}
+    public Node getRight(){return right;}
 }
