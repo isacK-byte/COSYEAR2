1dee80ccde0c3e381315dbd31c2a03bbacc2bc77
isacK-byte <4460799@myuwc.ac.za>
2026-04-21 22:30:35 +0200

    creating search function

--- 4460799P22/BinarySearchTree.java
+++ 4460799P22/BinarySearchTree.java
@@ -1,15 +1,29 @@
 //Binary Search Tree class
-class Node {
+ class Node {
     private int data;
     private Node left;
     private Node right;
 
     public Node(){
-        this.data = 0;
+        this.data = 0;          //default constructor
         this.left = null;
         this.right = null;
 
     }
+
+    public Node(int data, Node left, Node right){   //loaded constructor
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void setData(int data){this.data = data;}    //setters
+    public void setLeft(Node left){this.left = left;}
+    public void setRight(Node right){this.right = right;}
+                                            //getters
+    public int getData(){return data;}
+    public Node getLeft(){return left;}
+    public Node getRight(){return right;}
 }
 
 
@@ -17,23 +31,33 @@ class BinarySearchTree {
     private Node root;
     private int size;
 
-    public BinarySearchTree(){
+    public BinarySearchTree(){      //default constructor
         this.root = null;
         this.size = 0;
     }
 
-    public BinarySearchTree(Node root, int size){
+    public BinarySearchTree(Node root, int size){  //loaded constructor 
         this.root = root;
         this.size = size;
 
     }
 
-    public void setRoot(Node root){this.root = root;}
-    public void setSize(int size){this.size = size;}
+    public void setRoot(Node root){this.root = root;}       //setters
+    public void setSize(int size){this.size = size;}        
 
-    public Node getRoot(){return root;}
+    public Node getRoot(){return root;}             //getters
     public int getSize(){return size;}
-    
+
+    public boolean search(int data){
+        return complexSearch( root, data);
+    }
+
+    private boolean complexSearch(Node n, int data){
+        if (n == null) return false;
+        else if ( n.getData() == data) return true;
+        else if ( data < n.getData() ) return complexSearch(n.getLeft(), data);
+        else if (data > n.getData()) return complexSearch(n.getRight(), data);
+    }
 
     
 
--- 4460799P22/Node.java
+++ 4460799P22/Node.java
@@ -5,22 +5,22 @@
     private Node right;
 
     public Node(){
-        this.data = 0;
+        this.data = 0;          //default constructor
         this.left = null;
         this.right = null;
 
     }
 
-    public Node(int data, Node left, Node right){
+    public Node(int data, Node left, Node right){   //loaded constructor
         this.data = data;
         this.left = left;
         this.right = right;
     }
 
-    public void setData(int data){this.data = data;}
+    public void setData(int data){this.data = data;}    //setters
     public void setLeft(Node left){this.left = left;}
     public void setRight(Node right){this.right = right;}
-
+                                            //getters
     public int getData(){return data;}
     public Node getLeft(){return left;}
     public Node getRight(){return right;}
