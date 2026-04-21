20de76a51e865ae2eae7fe19d46b3c7461e77f4b
isacK-byte <4460799@myuwc.ac.za>
2026-04-21 20:50:38 +0200

    Getters and setters for Node and BinarySearchTree class

--- 4460799P22/BinarySearchTree.java
+++ 4460799P22/BinarySearchTree.java
@@ -1,4 +1,18 @@
 //Binary Search Tree class
+class Node {
+    private int data;
+    private Node left;
+    private Node right;
+
+    public Node(){
+        this.data = 0;
+        this.left = null;
+        this.right = null;
+
+    }
+}
+
+
 class BinarySearchTree {
     private Node root;
     private int size;
@@ -7,6 +21,20 @@ class BinarySearchTree {
         this.root = null;
         this.size = 0;
     }
+
+    public BinarySearchTree(Node root, int size){
+        this.root = root;
+        this.size = size;
+
+    }
+
+    public void setRoot(Node root){this.root = root;}
+    public void setSize(int size){this.size = size;}
+
+    public Node getRoot(){return root;}
+    public int getSize(){return size;}
+    
+
     
 
 }
--- 4460799P22/Node.java
+++ 4460799P22/Node.java
@@ -16,4 +16,12 @@
         this.left = left;
         this.right = right;
     }
+
+    public void setData(int data){this.data = data;}
+    public void setLeft(Node left){this.left = left;}
+    public void setRight(Node right){this.right = right;}
+
+    public int getData(){return data;}
+    public Node getLeft(){return left;}
+    public Node getRight(){return right;}
 }
