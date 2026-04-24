1a1d85d7c76c91e1735b9be4114d55c8eadda8de
isacK-byte <4460799@myuwc.ac.za>
2026-04-24 21:02:13 +0200

    removed node class from binarysearchtree class as my compiler suddenly recognises the node.java file lol

--- 4460799P22/BinarySearchTree.java
+++ 4460799P22/BinarySearchTree.java
@@ -181,36 +181,3 @@ class BinarySearchTree {
 
 
 
-class Node {
-    private int data;
-    private Node left;
-    private Node right;
-
-    public Node(){
-        this.data = 0;          //default constructor
-        this.left = null;
-        this.right = null;
-
-    }
-
-    public Node(int data, Node left, Node right){   //loaded constructor
-        this.data = data;
-        this.left = left;
-        this.right = right;
-    }
-
-    public Node(int data){
-        this.data = data;
-        this.left = null;
-        this.right = null;
-
-    }
-
-    public void setData(int data){this.data = data;}    //setters
-    public void setLeft(Node left){this.left = left;}
-    public void setRight(Node right){this.right = right;}
-                                            //getters
-    public int getData(){return data;}
-    public Node getLeft(){return left;}
-    public Node getRight(){return right;}
-}
